659ff7d320d2f187b417fb7ee2880ddf2b725460
Siyabonga Jiyane <siyabonga.jiyane@vumatel.co.za>
2026-09-11 08:41:58 +0200

    myEC2-2

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "MyEC2" {
     ami = "ami-0360c520857e3138f"
     instance_type = "t3.micro"
-
+}
 #     tags = {
 #         Name = "Which is Which"
 #     }
